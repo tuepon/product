8bbc54c1ada2b03cf089681ac7a9bdf70b21a873
tuepon <tuepon@gmail.com>
2017-07-23 15:30:08 +0900

    画像の入れ替えが実行できるようpro_edit_done.phpを改造

--- pro_edit_done.php
+++ pro_edit_done.php
@@ -14,6 +14,8 @@ try
 $pro_code=$_POST['code'];
 $pro_name=$_POST['name'];
 $pro_price=$_POST['price'];
+$pro_gazou_name_old=$_POST['pro_gazou_name_old'];
+$pro_gazou_name=$_POST['gazou_name'];
 
 $pro_code=htmlspecialchars($pro_code,ENT_QUOTES,'UTF-8');
 $pro_name=htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');

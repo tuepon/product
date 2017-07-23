ef11b6a501e1ee6a1f1f1d91955499c314107668
tuepon <tuepon@gmail.com>
2017-07-23 15:08:14 +0900

    商品修正画面で画像を扱えるようにpro_edit.phpを修正

--- pro_edit.php
+++ pro_edit.php
@@ -19,7 +19,7 @@ $password='123456';
 $dbh=new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='SELECT name,price FROM mst_product WHERE code=?';
+$sql='SELECT name,price,gazou FROM mst_product WHERE code=?';
 $stmt=$dbh->prepare($sql);
 $data[]=$pro_code;
 $stmt->execute($data);
@@ -27,9 +27,19 @@ $stmt->execute($data);
 $rec=$stmt->fetch(PDO::FETCH_ASSOC);
 $pro_name=$rec['name'];
 $pro_price=$rec['price'];
+$pro_gazou_name_old=$rec['gazou'];
 
 
 $dbh=null;
+
+if($pro_gazou_name_old=='')
+{
+    $disp_gazou='';
+}
+else
+{
+    $disp_gazou='<img src="./gazou/'.$pro_gazou_name_old.'">';
+}
 
 }
 catch(Exception $e)
@@ -46,12 +56,18 @@ catch(Exception $e)
 <?php print $pro_code; ?>
 <br>
 <br>
-<form method="post" action="pro_edit_check.php">
+<form method="post" action="pro_edit_check.php" enctype="multipart/form-data">
 <input type="hidden" name="code" value="<?php print $pro_code; ?>">
+<input type="hidden" name="gazou_name_old" value="<?php print $pro_gazou_name_old; ?>">
 商品名<br>
 <input type="text" name="name" style="width:200px" value="<?php print $pro_name; ?>"><br>
 価格<br>
 <input type="text" name="price" style="width:50px" value="<?php print $pro_price; ?>">円<br>
+<br>
+<?php print $disp_gazou; ?>
+<br>
+画像を選んでください。<br>
+<input type="file" name="gazou" style="width:400px"><br>
 <br>
 <input type="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
